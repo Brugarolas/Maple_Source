5d50741bfffcd20400e5a87a32a8e5d87349fb77
You_M <1055965862@qq.com>
2023-04-02 16:37:25 +0800

    add font render

---
+++ me/youm/rocchi/core/ui/font/FontLoaders.java
@@ -0,0 +1,49 @@
+/*
+ * Decompiled with CFR 0_132.
+ */
+package me.youm.rocchi.core.ui.font;
+
+import java.awt.Font;
+import java.io.InputStream;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+
+public abstract class FontLoaders {
+    public static CFontRenderer comfortaaR16 = new CFontRenderer(FontLoaders.getComfortaa(16,Roughness.Regular), true, true);
+    public static CFontRenderer comfortaaR18 = new CFontRenderer(FontLoaders.getComfortaa(18,Roughness.Regular), true, true);
+    public static CFontRenderer comfortaaT16 = new CFontRenderer(FontLoaders.getComfortaa(16,Roughness.THIN), true, true);
+    public static CFontRenderer comfortaaT18 = new CFontRenderer(FontLoaders.getComfortaa(18,Roughness.THIN), true, true);
+    public static CFontRenderer comfortaaB16 = new CFontRenderer(FontLoaders.getComfortaa(16,Roughness.Bold), true, true);
+    public static CFontRenderer comfortaaB18 = new CFontRenderer(FontLoaders.getComfortaa(18,Roughness.Bold), true, true);
+    public static CFontRenderer comfortaaB30 = new CFontRenderer(FontLoaders.getComfortaa(30,Roughness.Bold), true, true);
+
+    private static Font getComfortaa(int size,Roughness roughness) {
+        Font font;
+        try {
+            InputStream is = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("Rocchi/font/comfortaa_"+roughness.text+".ttf")).getInputStream();
+            font = Font.createFont(0, is);
+            font = font.deriveFont(0, size);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error loading font");
+            font = new Font("default", 0, size);
+        }
+        return font;
+    }
+    public enum Roughness{
+        THIN("thin"),
+        Regular("regular"),
+        Bold("bold");
+
+        private final String text;
+        Roughness(String text){
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+    }
+}
+
